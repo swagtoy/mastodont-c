078ac6bc1ae64c75938469a6a695d97fd9438a3c
Swagtoy <me@ow.swag.toys>
2022-03-10 04:36:11 +0000

    Mastodont request header

---
+++ include/mastodont_request.h
@@ -0,0 +1,37 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MASTODONT_REQUEST_H
+#define MASTODONT_REQUEST_H
+#include "mastodont.h"
+#include "mastodont_types.h"
+#include "mastodont_fetch.h"
+
+struct mastodont_request_args
+{
+    struct mstdnt_storage* storage;
+    char* url;
+    struct _mstdnt_query_param* params_query;
+    size_t params_query_len;
+    struct _mstdnt_query_param* params_post;
+    size_t params_post_len;
+    CURLoption request_type;
+    void* args;
+    int (*callback)(struct mstdnt_fetch_results* results, void*);
+};
+
+int mastodont_request(mastodont_t* data, struct mastodont_request_args* args);
+
+#endif /* MASTODONT_REQUEST_H */
