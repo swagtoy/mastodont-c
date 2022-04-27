10d2743f77c19cf3088ba36a8bb3e9f7eabdcf68
Swagtoy <me@ow.swag.toys>
2022-04-27 18:36:50 +0000

    Handle multiple errors (somewhat)

--- src/error.c
+++ src/error.c
@@ -16,6 +16,18 @@
 #include <mastodont_query.h>
 #include <mastodont_json_helper.h>
 
+static void _mstdnt_val_errors_call(cJSON* v, void* _type)
+{
+    char** type = _type;
+    if (v->child && v->child->valuestring)
+        *type = v->child->valuestring;
+    else
+    {
+        *type = NULL;
+        return;
+    }
+}
+
 int mstdnt_check_error(struct mstdnt_fetch_results* results,
                        struct mstdnt_storage* storage)
 {
@@ -28,8 +40,10 @@ int mstdnt_check_error(struct mstdnt_fetch_results* results,
     storage->error = NULL;
     storage->error_description = NULL;
 
+    /* TODO I have no idea why multiple errors can be returned */
     struct _mstdnt_val_ref refs[] = {
         { "error", &(storage->error), _mstdnt_val_string_call },
+        { "errors", &(storage->error), _mstdnt_val_errors_call },
         { "error_description", &(storage->error_description), _mstdnt_val_string_call },
     };
     
