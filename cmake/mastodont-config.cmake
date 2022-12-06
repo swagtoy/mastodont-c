6e8f0292a8ac31687191e15fb5d6263073609d67
Swagtoy <me@ow.swag.toys>
2022-12-06 13:04:33 +0000

    Mastodont-config

---
+++ cmake/mastodont-config.cmake
@@ -0,0 +1,11 @@
+# Get CFLAGS
+set(MASTODONT_FOUND)
+set(MASTODONT_VERSION 0.1)
+
+# Get parent directory of this config file
+
+set(MASTODONT_INCLUDE_DIRS
+  "${CMAKE_CURRENT_LIST_DIR}/../include"
+  "${CMAKE_CURRENT_LIST_DIR}/../libs"
+)
+set(MASTODONT_LIBRARIES "mastodont")
