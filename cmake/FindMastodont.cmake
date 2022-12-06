ad1d9c310dce712fb2bb8b561f8367178d0a4cf2
Swagtoy <me@ow.swag.toys>
2022-12-06 02:47:14 +0000

    CMake

---
+++ cmake/FindMastodont.cmake
@@ -0,0 +1,18 @@
+#[=======================================================================[.rst:
+FindMastodont
+--------
+
+Find the Mastodont library.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This module defines the following variables:
+
+``CURL_FOUND``
+``CURL_INCLUDE_DIRS``
+``CURL_LIBRARIES``
+``CURL_VERSION_STRING``
+
+#]=======================================================================]
+
