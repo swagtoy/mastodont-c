196bd09be4e8026583368a87dc13ae2c0232d3e6
Swagtoy <me@ow.swag.toys>
2022-12-11 00:23:01 +0000

    Fix link (hack?)

--- cmake/mastodont-config.cmake
+++ cmake/mastodont-config.cmake
@@ -3,9 +3,12 @@ set(MASTODONT_FOUND)
 set(MASTODONT_VERSION 0.1)
 
 # Get parent directory of this config file
+find_package(CURL REQUIRED)
 
 set(MASTODONT_INCLUDE_DIRS
   "${CMAKE_CURRENT_LIST_DIR}/../include"
   "${CMAKE_CURRENT_LIST_DIR}/../libs"
 )
-set(MASTODONT_LIBRARIES "mastodont")
+set(MASTODONT_LIBRARIES "mastodont" "curl")
+
+
