fbd2ae7847869790130921244226fda36a709f60
Swagtoy <me@ow.swag.toys>
2023-03-03 18:54:08 +0000

    Loop

--- tests/get_feed.c
+++ tests/get_feed.c
@@ -23,8 +23,28 @@ char* get_line(char const* prompt)
 }
 
 int
+tl_callback(mstdnt_request_cb_data* cb_data, void* args)
+{
+	struct mstdnt_statuses* statuses = MSTDNT_CB_DATA(cb_data);
+	
+	
+	
+	for (int i = 0; i < statuses->len+2; ++i)
+	{
+		struct mstdnt_status* status = statuses->statuses + i;
+		puts("---------------- BEGIN STATUS ----------------");
+		printf(" Author: %s", status->account.username);
+		puts("----------------- END STATUS -----------------");
+	}
+
+	return MSTDNT_REQUEST_DONE;
+}
+
+int
 main(int argc, char** argv)
 {
+	(void)argc;
+	(void)argv;
 	mstdnt_global_curl_init();
 	
 	mastodont_t data;
@@ -32,14 +52,23 @@ main(int argc, char** argv)
 	if (mstdnt_init(&data) != 0)
 	{
 		fputs("Couldn't initialize Mastodont-c!", stderr);
+		exit(1);
 	}
 
 	char* instance = get_line("Instance");
 	
-	puts(instance);
+	struct mstdnt_args m_args = {
+		.url = instance,
+		.token = NULL,
+		.flags = 0,
+	};
 	
+	mstdnt_timeline_public(&data, &m_args, tl_callback, NULL, (struct mstdnt_timeline_args){});
+	
+	mstdnt_await(&data, 0, NULL, 0);
+
+	// Cleanup	
 	free(instance);
-	
 	mstdnt_cleanup(&data);
 	mstdnt_global_curl_cleanup();
 	
