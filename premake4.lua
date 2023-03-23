af1168859cdb22fd4e4e6e875813f6883176f24b
Swagtoy <me@ow.swag.toys>
2023-03-23 14:19:04 +0000

    pkg-config file

--- premake4.lua
+++ premake4.lua
@@ -8,9 +8,9 @@ language("C");
 files { "include/*.h", "src/*.c" };
 includedirs { "include/" };
 
-configuration { "linux", "bsd", "gmake" }
-	linkoptions { "`curl-config --libs`" }
-	linkoptions { "`pkg-config --libs cjson`" }
+configuration { "linux", "bsd", "gmake" };
+	linkoptions { "`curl-config --libs`" };
+	linkoptions { "`pkg-config --libs cjson`" };
 
 configuration { "Debug" };
 	defines { "DEBUG" };
@@ -20,3 +20,19 @@ configuration { "Release" };
 	defines { "NDEBUG" };
 	flags("Optimize");
 -- END Mastodont-c
+
+local prefix = os.getenv("PREFIX") or "/usr/local";
+local pkgconfig_path = os.getenv("PKG_CONFIG_PATH") or
+                       (os.isdir(prefix .. "/pkgconfig") and prefix .. "/pkgconfig") or
+                       prefix .. "/share/pkgconfig";
+
+newaction {
+	trigger = "install",
+	description = "install the library",
+	execute = function()
+		os.copyfile("libmastodont.a", prefix .. "/lib");
+		os.execute("install -d include/ " .. prefix .. "/include");
+		os.mkdir(pkgconfig_path);
+		os.copyfile("mastodont.pc", pkgconfig_path .. "/mastodont.pc");
+	end
+}
